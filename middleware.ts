887b8260b3822e2a99a3b792b87185aa1481368c
Dhanush Kumar M <iamdk353@gmail.com>
2024-11-20 20:12:03 +0530

    middel ware fix

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/",
-  "/profile",
-  "/api(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
